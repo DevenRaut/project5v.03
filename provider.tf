6e9b331ab14c84463eb81cf0145a4b56ab7a038c
DevenRaut <91963968+DevenRaut@users.noreply.github.com>
2024-08-13 01:20:42 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA2UC3FJGRFK2EMUXHD"
+  secret_key = "59SuezwF5G7hYRfU8EQML51M/Z4A10PD/H+k/6wjD"
+}
